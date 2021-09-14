ca3600d518328967bdb7ad4092f30bc03f02121d
Dung (Jun) Trinh <dungquoctrinh@users.noreply.github.com>
2021-09-13 21:23:40 -0700

    first commit

---
+++ backend/database/db.js
@@ -0,0 +1,5 @@
+//using the local database
+module.exports = {
+   //db: 'mongodb://localhost:27017/meandb'
+   db: 'mongodb+srv://admin:admin123@cluster0.eqztm.mongodb.net/meandb?retryWrites=true&w=majority'
+};
